99dd8dfb507083bf4e5f100d49603cc675949497
Juan Jose Mejía Moscol <jujosemoscol@gmail.com>
2025-10-30 21:02:26 +0000

    para nueva coleccion esto https://www.iese.edu/es/insight/wp-content/upl

--- next.config.ts
+++ next.config.ts
@@ -45,6 +45,12 @@ const nextConfig: NextConfig = {
         hostname: 'hips.hearstapps.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.iese.edu',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
