02f2fd0e08b1ead241c6d9c7ee20174948ea3869
Juan Jose Mejía Moscol <jujosemoscol@gmail.com>
2025-10-30 21:40:38 +0000

    mas grande (_for element <Logo>_)

--- src/components/login-dialog.tsx
+++ src/components/login-dialog.tsx
@@ -47,7 +47,7 @@ export function LoginDialog({ children }: { children: React.ReactNode }) {
         <DialogHeader className="text-center items-center">
            <div className="inline-block mx-auto mb-4">
             <Link href="/" onClick={() => setOpen(false)}>
-                <Logo className="h-12 w-12" />
+                <Logo className="h-16 w-16" />
             </Link>
           </div>
           <DialogTitle className="text-2xl font-headline">
