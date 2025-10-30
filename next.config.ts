b6965d0733a7edb7f6695c685f5f28e8b0984bbf
Juan Jose Mejía Moscol <jujosemoscol@gmail.com>
2025-10-30 20:39:19 +0000

    https://www.kneipp.com/dw/image/v2/BGQM_PRD/on/demandware.static/-/Sites

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.kneipp.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
