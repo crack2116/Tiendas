01233efaeda3c8c2251f2c07e73ff900fda3b0ac
Juan Jose Mejía Moscol <jujosemoscol@gmail.com>
2025-10-31 04:05:04 +0000

    hazlo mas rapido demoras

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,22 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs, writeBatch } from 'firebase/firestore';
-import { config } from 'dotenv';
+import { firebaseConfig } from '../src/firebase/config';
 import placeholderImagesData from '../src/lib/placeholder-images.json';
 
-// Load environment variables from .env file
-config({ path: './.env.local' });
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-
 if (!firebaseConfig.projectId) {
-  console.error("Firebase project ID is not defined in your .env file.");
+  console.error("Firebase project ID is not defined in your firebase/config.ts file.");
   process.exit(1);
 }
 
