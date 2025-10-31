282d329dd3465ad6f011cdf3ead14311e9aefe4b
Juan Jose Mejía Moscol <jujosemoscol@gmail.com>
2025-10-31 04:59:47 +0000

    que no vez el espacio que sobra ajustalo bien

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -123,9 +123,9 @@ export default function AdminLayout({
             </SidebarMenu>
           </SidebarFooter>
         </Sidebar>
-        <div className="flex-1 bg-muted/40 p-4 sm:p-6 md:p-8 overflow-auto">
+        <SidebarInset className="flex-1 bg-muted/40 p-4 sm:p-6 md:p-8">
             {children}
-        </div>
+        </SidebarInset>
       </div>
     </SidebarProvider>
   )
