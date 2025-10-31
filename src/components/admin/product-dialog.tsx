f6c4aa5e65e3827e9122bb86369aefb12f636385
Juan Jose Mejía Moscol <jujosemoscol@gmail.com>
2025-10-31 13:42:58 +0000

    ponlo ancho esto

--- src/components/admin/product-dialog.tsx
+++ src/components/admin/product-dialog.tsx
@@ -25,7 +25,7 @@ export function ProductDialog({ isOpen, onClose, product }: ProductDialogProps) 
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="sm:max-w-[625px]">
+      <DialogContent className="sm:max-w-4xl">
         <DialogHeader>
           <DialogTitle>{dialogTitle}</DialogTitle>
           <DialogDescription>{dialogDescription}</DialogDescription>
